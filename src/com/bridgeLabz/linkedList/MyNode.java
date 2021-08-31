289216c36a1f061d0c7619ceab0626d85a428b66
ajaybarath <ajaybarath3@gmail.com>
2021-08-31 13:40:37 +0530

    ADDED: implementation of INode

--- src/com/bridgeLabz/linkedList/MyNode.java
+++ src/com/bridgeLabz/linkedList/MyNode.java
@@ -1,5 +1,34 @@
 package com.bridgeLabz.linkedList;
 
-public class MyNode {
+public class MyNode<K> implements INode<K>{
+
+	private K key;
+	private INode next;
+		
+	@Override
+	public K getKey() {
+		// TODO Auto-generated method stub
+		return key;
+	}
+
+	@Override
+	public void setKey(K key) {
+		// TODO Auto-generated method stub
+		this.key = key;
+	}
+
+	@Override
+	public INode<K> getNext() {
+		// TODO Auto-generated method stub
+		return next;
+	}
+
+	@Override
+	public void setNext(INode<K> next) {
+		// TODO Auto-generated method stub
+		this.next = next;
+	}
+	
+	
 
 }
