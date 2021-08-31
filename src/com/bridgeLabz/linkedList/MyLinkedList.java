73fc4a4d6d592eb8dddc7b3ed5282ef47eb35550
ajaybarath <ajaybarath3@gmail.com>
2021-08-31 14:11:13 +0530

    ADDED: main class to create node

--- src/com/bridgeLabz/linkedList/MyLinkedList.java
+++ src/com/bridgeLabz/linkedList/MyLinkedList.java
@@ -2,4 +2,14 @@ package com.bridgeLabz.linkedList;
 
 public class MyLinkedList {
 	
+	public static void main(String args[]) {
+
+		INode node1 = new MyNode(56);
+		INode node2 = new MyNode(30);
+		INode node3 = new MyNode(70);
+		
+		node1.setNext(node2);
+		node2.setNext(node3);
+	}
+	
 }
